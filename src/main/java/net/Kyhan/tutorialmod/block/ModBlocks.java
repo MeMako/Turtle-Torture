e054ebd9843ab8d27c5b815b3fe5b43d63e5d8c1
Kyhan <kyhanb@icloud.com>
2022-04-01 17:17:48 +1000

    it now exists and there files

---
+++ src/main/java/net/Kyhan/tutorialmod/block/ModBlocks.java
@@ -0,0 +1,11 @@
+package net.Kyhan.tutorialmod.block;
+
+import net.Kyhan.tutorialmod.TutorialMod;
+
+public class ModBlocks {
+
+    //up to 1:20 in blocks vid
+    public static void registerModBlocks() {
+        TutorialMod.LOGGER.info("Registering ModBlocks for " + TutorialMod.MOD_ID);
+    }
+}

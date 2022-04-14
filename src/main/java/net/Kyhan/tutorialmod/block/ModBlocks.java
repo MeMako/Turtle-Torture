304274527be9bbc0959f27d76dd3f056eb0d0e13
Kyhan <kyhanb@icloud.com>
2022-04-14 13:55:52 +1000

    added part of blocks code and stuff

--- src/main/java/net/Kyhan/tutorialmod/block/ModBlocks.java
+++ src/main/java/net/Kyhan/tutorialmod/block/ModBlocks.java
@@ -1,8 +1,25 @@
 package net.Kyhan.tutorialmod.block;
 
 import net.Kyhan.tutorialmod.TutorialMod;
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.minecraft.block.Block;
+import net.minecraft.item.BlockItem;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.registry.Registry;
+
 
 public class ModBlocks {
+//2:55 in vid
+    private static Block registerBlock(String name, Block block, ItemGroup group){
+        registerBlockItem(name, block, group);
+    }
+
+    private static Item registerBlockItem(String name, Block block, ItemGroup group) {
+        return Registry.register(Registry.ITEM, new Identifier(TutorialMod.MOD_ID, name),
+                new BlockItem(block, new FabricItemSettings().group(group)))
+    }
 
     //up to 1:20 in blocks vid
     public static void registerModBlocks() {
